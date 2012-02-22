001ce7e2d84137efe4f710df7239b8d56d910a8b
Brandon John-Freso <brandonjf@atomicflowtech.com>
2012-02-21 20:06:25 -0500

    Pushing the assets and Bootstrap

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,2 +1,12 @@
-Welcome to Bogo!
+<head>
+	<meta charset="utf-8">
+	<title>Connector</title>
+	<link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.css">
+	<script type="text/javascript" src="/assets/js/jquery-1.7.1.min.js"></script>
+	<script type="text/javascript" src="/assets/js/bootstrap.js"></script>
+
+</head>
+
+Welcome to BoGo!
 <a href="<?php echo site_url('signup'); ?>">Sign Up!</a>
+
