dece6abe7a4c11d9c5d4d871635947d2f52dbc5b
Brandon John-Freso <brandonjf@atomicflowtech.com>
2012-02-28 20:53:35 -0500

    Commiting design cover

--- application/views/authentication/consumer/signin.php
+++ application/views/authentication/consumer/signin.php
@@ -1,14 +1,27 @@
 <div class="row">
     <?php echo validation_errors(); ?>
-    <form id="signinForm" method="post" action="<?php echo site_url('signin') ?>" class="form-horizontal well span3">
-        <fieldset>
-            <legend>Sign in!</legend>
-            <label>Username</label>
-            <input type="text" id="username" name="username" class="span3" placeholder="Enter Here..."/>
-            <label>Password</label>
-            <input type="password" id="password" name="password" class="span3" placeholder="Enter Here..."/>
-        </fieldset>
-        <button type="submit" class="btn">Submit</button>
-    </form>
+    <div id="signinFormBackground" class="span">
+
+        <div class="row" >
+
+            <div class="span4" id="signinFormAside">
+               The Bogo Experience. We give you a chance to lower the price on items that you want by performing simple tasks. The better you do, the better the price you'll get!
+            </div>
+
+            <form id="signinForm" method="post" action="<?php echo site_url('signin') ?>" class="form-horizontal well span4">
+                <fieldset>
+                    <legend>Sign in!</legend>
+                    <label>Username</label>
+                    <input type="text" id="username" name="username" class="span3" placeholder="Enter Here..."/>
+                    <label>Password</label>
+                    <input type="password" id="password" name="password" class="span3" placeholder="Enter Here..."/>
+                </fieldset>
+                <button type="submit" class="btn">Login</button>
+            </form>
+
+            
+        </div>
+
+    </div>
 </div>
 <!-- end row -->
--- assets/js/starter.js
+++ assets/js/starter.js
@@ -1,7 +1,11 @@
 jQuery(function($){
 				
 				$.supersized({
-					slides  :  	[ {image : '/assets/image/background/fabdezbg.jpg', title : 'Image Credit: Maria Kazvan'} ]
+					start_slide             :   0,			// Start slide (0 is random)
+					slides  :  	[ {image : '/assets/image/background/fabdezbg.jpg', title : 'Image Credit: Fab Design'} ,
+								  {image : '/assets/image/background/kitchen.jpg', title : 'Image Credit: Fab Design'} ,
+								  {image : '/assets/image/background/kittyEdit.jpg', title : 'Kitty BG'} ,
+					]
 				});
 
 		    });
