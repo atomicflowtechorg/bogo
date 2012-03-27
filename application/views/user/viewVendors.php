ae7335776222bb9af8baa18365041d209cda7437
Brandon John-Freso <brandonjf@atomicflowtech.com>
2012-03-26 21:09:44 -0400

    Commiting some vendor listing changes aagain

--- application/views/user/viewVendors.php
+++ application/views/user/viewVendors.php
@@ -4,17 +4,16 @@
 // var_dump($vendors);
 
 foreach($vendors as $vendor){
-    print_r($vendor->name);
     echo "<div class='vendorCard'>
     <div class='vendorDetailsContainer'>
         details
     </div>
     <div class='vendorNameContainer'>
         <div class='moreTriangle'><img src='/assets/image/icon/moreTriangle.png' alt=''></div>
-        <h3 class='vcVendorName'>Nike Outlet</h3>
+        <h3 class='vcVendorName'>$vendor->name</h3>
     </div>
     <div class='karmaIndicatorContainer'>
-        <div class='karmaIndicator'><span class='karmaPoints'>224</span></div>
+        <div class='karmaIndicator'><span class='karmaPoints'>$vendor->karrrma</span></div>
     </div>
 </div>";
 }
@@ -29,9 +28,7 @@ foreach($vendors as $vendor){
 ?>
 
 
-
-<!-- 
-<div class="vendorCard">
+<!-- <div class="vendorCard">
     <div class="vendorDetailsContainer">
         details
     </div>
